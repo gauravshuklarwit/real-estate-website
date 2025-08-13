15768be22f3ebc9afbc854944db3b2ce5d804450
Gaurav Shukla <shukla.gaurav@rwit.io>
2025-08-13 19:37:42 +0530

    feat: render Header and HeroSection component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Header } from "@/components/header";
+import { HeroSection } from "@/components/hero-section";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <div className="text-background bg-gradient-to-br from-[#464646] to-[#222222]">
+          <Header />
+          <div className="mx-auto max-w-[90rem]">
+            <HeroSection />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
