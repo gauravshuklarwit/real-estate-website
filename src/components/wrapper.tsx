916b54a94d2443ff9638db0c11eaf596d6123d21
Gaurav Shukla <shukla.gaurav@rwit.io>
2025-08-13 11:47:48 +0530

    feat: add Wrapper component

---
+++ src/components/wrapper.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+export function Wrapper({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return <div className={cn("max-w-6xl mx-auto px-4 md:px-6")}>{children}</div>;
+}
