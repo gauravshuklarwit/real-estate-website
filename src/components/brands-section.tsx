3aee5100ddaac334e8ec59090504b66c3f4ff3c8
Gaurav Shukla <shukla.gaurav@rwit.io>
2025-08-14 12:15:49 +0530

    feat: make brand images responsive

--- src/components/brands-section.tsx
+++ src/components/brands-section.tsx
@@ -17,16 +17,15 @@ export function BrandsSection() {
   return (
     <section>
       <div className="container py-14">
-        <ul className="grid place-items-center gap-8 sm:grid-cols-3 xl:grid-cols-5 xl:gap-24">
-          {/* <ul className="flex flex-wrap justify-between items-center gap-8 sm:items-center"> */}
+        <ul className="grid place-items-center gap-8 sm:grid-cols-3 sm:gap-12 md:grid-cols-4 lg:grid-cols-5 xl:gap-24">
           {brands?.map((brand) => (
             <li key={brand?.name}>
               <Image
                 src={brand?.image || "//brands/adira.png"}
                 alt={`${brand?.name} logo`}
-                width={500}
-                height={250}
-                className="h-auto w-full min-w-48"
+                width={220}
+                height={120}
+                className="w-full max-w-80"
               />
             </li>
           ))}
