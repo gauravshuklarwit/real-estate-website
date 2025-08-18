214d7f0967326249449e0cb81ee5635daeb7c986
gauravshuklarwit <shukla.gaurav@rwit.io>
2025-08-18 19:03:53 +0530

    feat: add slider on BrandsSection

--- src/components/brands-section.tsx
+++ src/components/brands-section.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 interface Brand {
   name?: string;
@@ -17,19 +18,21 @@ export function BrandsSection() {
   return (
     <section>
       <div className="container py-14">
-        <ul className="grid place-items-center gap-8 sm:grid-cols-3 sm:gap-12 md:grid-cols-4 lg:grid-cols-5 xl:gap-24">
-          {brands?.map((brand) => (
-            <li key={brand?.name}>
-              <Image
-                src={brand?.image || "//brands/adira.png"}
-                alt={`${brand?.name} logo`}
-                width={220}
-                height={120}
-                className="w-full max-w-80"
-              />
-            </li>
-          ))}
-        </ul>
+        <Marquee>
+          <ul className="flex items-center">
+            {brands?.map((brand) => (
+              <li key={brand?.name}>
+                <Image
+                  src={brand?.image || "//brands/adira.png"}
+                  alt={`${brand?.name} logo`}
+                  width={220}
+                  height={120}
+                  className="w-full max-w-80 px-4 sm:px-6 xl:px-12"
+                />
+              </li>
+            ))}
+          </ul>
+        </Marquee>
       </div>
     </section>
   );
