b0c5d753f5763e964a64e5448f730e1e7d35eb74
gauravshuklarwit <shukla.gaurav@rwit.io>
2025-08-19 16:55:12 +0530

    feat: improve responsiveness of architect profile section images

--- src/components/architect-profile-section.tsx
+++ src/components/architect-profile-section.tsx
@@ -8,31 +8,32 @@ export function ArchitectProfileSection() {
   return (
     <section id="architect-profile-section" className="py-16">
       <div className="container grid gap-13 lg:grid-cols-[60%_auto]">
-        <div className="relative grid md:grid-cols-4">
+        <div className="relative grid grid-cols-5 grid-rows-5 sm:grid-cols-4">
           <Image
             src="/architecture-image.svg"
             alt="Architecture image"
             width={535}
             height={570}
-            className="col-[1/-1] row-[1/4] md:col-[1/4] md:row-[1/2]"
+            className="col-[1/-2] row-[1/-2] size-full object-cover sm:row-[1/-1]"
           />
 
-          <div className="text-background absolute right-0 bottom-0 z-10 grid max-w-80 justify-items-center gap-2 bg-gradient-to-br from-[#474747] to-[#222222] px-4 py-8 md:static md:col-[3/5] md:row-[1/2] md:self-center">
+          <div className="text-background z-10 col-[2/-1] row-[3/-1] mt-auto grid max-h-fit justify-items-center gap-2 bg-gradient-to-br from-[#474747] to-[#222222] px-4 py-8 shadow-2xl sm:col-[3/-1] sm:row-[2/-2] sm:my-auto">
             <Image
               src="/architect-avatar.svg"
               alt="Architect avatar"
               width={100}
               height={100}
+              className="size-16"
             />
             <p className="mt-3 text-xl font-semibold">Dianne Russell</p>
-            <p className="text-center leading-relaxed text-[#E5E5E5]">
+            <p className="text-center leading-relaxed text-[#E5E5E5] max-sm:text-sm">
               More than 20 years of experience in the field architecture and has
               worked on project up to 100+
             </p>
           </div>
         </div>
 
-        <div className="-order-1 grid justify-items-center text-center md:justify-items-start md:text-left lg:order-1 lg:content-end">
+        <div className="grid justify-items-center text-center md:justify-items-start md:text-left lg:content-end">
           <h2 className="heading-2">
             Meet and talk with our best architecture
           </h2>
