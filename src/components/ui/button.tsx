11ccfe46ead665994ed389a263918e1971a682e2
gauravshuklarwit <shukla.gaurav@rwit.io>
2025-08-19 14:30:52 +0530

    feat: add hover states on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,16 +5,16 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex cursor-pointer items-center justify-center gap-2 whitespace-nowrap font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none",
+  "inline-flex cursor-pointer items-center justify-center gap-2 whitespace-nowrap font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 hover:[&_svg]:translate-x-1 duration-200 hover:ring-4 hover:ring-primary hover:ring-offset-3 hover:ring-offset-background",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 ",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         ghost: "hover:bg-primary hover:text-primary-foreground",
       },
       size: {
         default: "px-6 py-3 has-[>svg]:px-8 xl:px-10 xl:py-5",
-        icon: "p-4",
+        icon: "p-4 hover:[&_svg]:translate-0",
       },
     },
     defaultVariants: {
