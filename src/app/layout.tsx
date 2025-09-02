adb4c2bf370e080b903c2f7787e1ca57185f80bb
Gaurav Shukla <shukla.gaurav@rwit.io>
2025-09-02 18:28:31 +0530

    fix: remove redundant antialiased class from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} antialiased`}>
+    <html lang="en" className={`${poppins.variable}`}>
       <body>{children}</body>
     </html>
   );
